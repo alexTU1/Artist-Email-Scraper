87068d99b88efa63190203c52e0fc808724771a4
alexTU1 <95954794+alexTU1@users.noreply.github.com>
2022-04-24 19:08:52 -0700

    Update artists.js

--- artists.js
+++ artists.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var nodemailer = require('nodemailer');
 
 var takeFirstTwo = process.argv.splice(0,2); // gets rid of first two arguments
 var artistNames = process.argv; //array of artist names
@@ -21,11 +22,11 @@ request('http://www.popvortex.com/music/charts/top-rap-songs.php', function (err
             });
            // console.log(multiplExist);
 
-           const output = `
-           <h3>Your REQUEST!</h3>
-           <p>You chose the Artist(s): ${artistNames}</p>
-           
-           `;
+        //    const output = `
+        //    <h3>Your REQUEST!</h3>
+        //    <p>You chose the Artist(s): ${artistNames}</p>
+
+        //    `;
             
            if(i<chartLength && stringName.toLowerCase().includes(lowerCase) || stringSong.toLowerCase().includes(lowerCase)){  
                 //console.log("Artist(s) Name: " + stringName + "\nSong-Title    : " + stringSong);
@@ -42,9 +43,35 @@ request('http://www.popvortex.com/music/charts/top-rap-songs.php', function (err
                     console.log();
                     process.exit(1);
                 }
-                console.log();     
-           }
-       }); 
+                    console.log(); 
+
+                    const url = './credentials.json' //directory to credentials.json object (credentials.json file MUST be local)
+                    let json = require(url); //grabs credentials.json object 
+
+                    let transporter = nodemailer.createTransport({
+                        service: 'gmail',
+                        auth:{
+                            user: json.sender_email, //credential.json attribute(sender_email)
+                            pass: json.sender_password //credential.json attribute(sender_password)
+                        }
+                    });
+
+                    let mailOptions = {
+                        from: json.from, //credential.json attribute(from)
+                        to: json.to, //credential.json attribute(to)
+                        subject: "Your Artist(s) are/is: " + artistNames ,
+                        text: "Artist(s): " + stringName + '\n' + 
+                            "Song: " + stringSong
+                        } 
+                        
+                        transporter.sendMail(mailOptions, function(err, data){
+                            if(err){
+                                console.log(err.message);
+                            }else{
+                                console.log("Email has been successfully sent!!");
+                            }
+                        });
+            }    
+        }); 
 	}
 });
-
